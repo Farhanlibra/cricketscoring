b5036a502d208e577c28f8c8cc7a57b3a70e2e9e
Farhanlibra <137736801+Farhanlibra@users.noreply.github.com>
2025-04-25 17:01:10 +0500

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ function App() {
       </div>
 
       <div style={{ marginBottom: "1rem" }}>
-        <strong style={{fontSize:50}}>Score:</strong> {runs}/{wickets} ({formatOvers()} overs)
+        <strong style={{fontSize:50}}>Score:</strong> <span style={{fontSize:50}}> {runs}/{wickets} ({formatOvers()} overs </span>)
         <br />
         <strong>Batting:</strong>
         <ul>
